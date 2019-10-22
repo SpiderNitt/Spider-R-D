ad1d986397f06a51d8cd3077f14e4750f8b6e523
Abhishek L <abhishek122k@gmail.com>
2019-10-22 14:21:55 +0000

    Upload New File

---
+++ src/Navbar/projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from '../Navbar/navbar.js';
+import AutoGrid from './body.js';
+
+
+function ProjectsPage() {
+  return ( 
+    <div> 
+      <center>
+      <AutoGrid/><br/>
+      </center>      
+    </div>
+  );
+}
+
+export default ProjectsPage;
