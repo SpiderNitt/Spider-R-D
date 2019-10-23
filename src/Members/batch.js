6d197f7c9d715181bebe184e3868634d973d8b17
srn11 <srnandhini1108@gmail.com>
2019-10-23 21:09:37 +0530

    Spider Site Members Page

---
+++ src/Members/batch.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import './members.css'
+
+export default ({yearName , Year , onSelect}) => {
+
+  const index = yearName ? Year.findIndex(group => group === yearName) : 0
+
+  const onIndexSelect = (e, index) =>
+      onSelect(Year[index])
+  
+  return (
+    <Paper className='tabs'>
+      <Tabs
+        value={index}
+        onChange={onIndexSelect}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >        
+        <Tab label="All" />
+        <Tab label="2019-20" />
+        <Tab label="2020-21" />
+        <Tab label="2021-22" />        
+      </Tabs>
+    </Paper>
+  );
+}
