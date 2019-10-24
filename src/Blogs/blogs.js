80ae17cccda4034a5008e2fd232fcc20f2d4ebfb
AnjaneyaTripathi <anjaneya532000@gmail.com>
2019-10-24 20:52:57 +0530

    Revert "created blogs page"
    This reverts commit 36f4714fb4fb78497207e15cea5094aace132ca2.

--- src/Blogs/blogs.js
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import blogPage from "./components/blogPage";
-// import "./index.css";
-
-// ReactDOM.render(<blogPage />, document.getElementById("root"));
