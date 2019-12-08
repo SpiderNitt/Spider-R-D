ed6d277ca4250c2079d746ec02578d877cc086d6
barath99 <exploretechsbk@gmail.com>
2019-12-08 12:33:26 +0530

    bg temporarily - canvas correction pending

--- src/HomePage/Sparticle/sparticle.jsx
+++ src/HomePage/Sparticle/sparticle.jsx
@@ -1,16 +1,18 @@
 import React,{ Component } from "react";
 import "./sparticle.css";
-import "./indScript.js";
-
+// import "./indScript.js";
+//<canvas id="c"/> to be placed inside div...
+//<center>
+//    <img className="ima1" src="SpiderLogo.png"  /><br />
+//    <img className="ima2" src="navLogo.png"  />
+//</center>
 class Sparticle extends Component{
   render() {
     return (
       <div>
-				<canvas id="c"/>
-				    <center>
-				        <img className="ima1" src="SpiderLogo.png"  /><br />
-				        <img className="ima2" src="navLogo.png"  />
-				    </center>
+
+        <img src="bg-alt.jpg" style={{width:"100%",margin:"0"}}/>
+
 			</div>
     );
   }
