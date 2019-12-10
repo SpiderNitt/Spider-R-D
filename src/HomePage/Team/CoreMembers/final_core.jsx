b6f21cb314070f0417167802b660c098e413272b
VishalS99 <s-vishal1999@hotmail.com>
2019-12-10 20:38:02 +0530

    Refactored files in Homepage/members

--- src/HomePage/Team/CoreMembers/final_core.jsx
+++ src/HomePage/Team/CoreMembers/final_core.jsx
@@ -3,75 +3,85 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Grid } from "@material-ui/core";
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardMedia from "@material-ui/core/CardMedia";
 
 import LanguageIcon from "@material-ui/icons/Language";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import IconButton from "@material-ui/core/IconButton";
 import core from "./coreMembersData.json";
-import './corecard.css'
+import "./corecard.css";
 
 export default function CoreMembers() {
   return (
     <div id="core-main">
       <center>
-      <h1 className="core-header">Core Members</h1>  
-        <br/>    
-          <div>
-            <Grid
-              container
-              spacing={6}
-              alignItems="center"
-              justify="center"
-              className="coreGrid"
-            >
-              {core.map(members => (
-                <Grid item xs={12} sm={6} md={4} lg={3} xl={3} align="center" key={members.id}>
-                  <Card style={{maxWidth: 300}}>
-                    <CardActionArea>
-                        <CardMedia
-                        component="img"
-                        alt={members.Role}
-                        height="270"
-                        image={members.Photo}
-                        title={members.Role}
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {members.Name}
-                        </Typography>
-                        <Typography variant="h6" color="textSecondary">
+        <h1 className="core-header">Core Members</h1>
+        <br />
+        <div>
+          <Grid
+            container
+            spacing={6}
+            alignItems="center"
+            justify="center"
+            className="coreGrid"
+          >
+            {core.map(members => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                xl={3}
+                align="center"
+                key={members.id}
+              >
+                <Card style={{ maxWidth: 300 }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      alt={members.Role}
+                      height="270"
+                      image={members.Photo}
+                      title={members.Role}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {members.Name}
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
                         {members.Role}
-                        </Typography>
-                        </CardContent>
-                   
-                    <CardActions style={{marginLeft:'20%', marginTop:'-5%'}}>
-                        <a href={members.Website}>
+                      </Typography>
+                    </CardContent>
+
+                    <CardActions
+                      style={{ marginLeft: "20%", marginTop: "-5%" }}
+                    >
+                      <a href={members.Website}>
                         <IconButton aria-label="personalSite">
-                        <LanguageIcon />
+                          <LanguageIcon />
                         </IconButton>
-                        </a>
-                        <a href={members.LinkedIn}>
+                      </a>
+                      <a href={members.LinkedIn}>
                         <IconButton aria-label="linkedIn">
-                        <LinkedInIcon />
+                          <LinkedInIcon />
                         </IconButton>
-                        </a>
-                        <a href={members.Github}>
+                      </a>
+                      <a href={members.Github}>
                         <IconButton aria-label="gitHubLink">
-                        <GitHubIcon />
+                          <GitHubIcon />
                         </IconButton>
-                        </a>
+                      </a>
                     </CardActions>
-                    </CardActionArea>
-                    </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </div>
-       
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </center>
     </div>
   );
--- src/HomePage/homepage.jsx
+++ src/HomePage/homepage.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Footer from '../Footer/Footer';
-//import CoreMembers from './Team/CoreMembers/coreMemberCards';
 import CoreMembers from './Team/CoreMembers/final_core';
 import Team from './Team/Team/team';
 import About from './About/about';
