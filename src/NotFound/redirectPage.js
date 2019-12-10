8be1de793c2cf21c67be7da5c8555825bd712bd7
VishalS99 <s-vishal1999@hotmail.com>
2019-12-10 23:13:36 +0530

    Refined 404 Page

--- src/NotFound/redirectPage.js
+++ src/NotFound/redirectPage.js
@@ -4,15 +4,15 @@ import './redirectPage.css'
 
 const NotFound = () => {
     return <div id="notfound">
-    <div class="notfound">
-        <div class="notfound-404">
-            <h1>Oops!</h1>
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>Oops!</h1>
+            </div>
+            <h2>404 - Page not found</h2>
+            <p>The page you are looking for can't be found!</p>
+            <a href="/">Go To Homepage</a>
         </div>
-        <h2>404 - Page not found</h2>
-        <p>The page you are looking for can't be found!</p>
-        <a href="/">Go To Homepage</a>
     </div>
-</div>
 }
 
 export default NotFound;
--- src/index.js
+++ src/index.js
@@ -2,28 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Blogs from './Blogs/blogPage';
 import HomePage from './HomePage/homepage';
 import Projects from './Projects/projects'
 import Members from './Members/new_members';
-import NotFound from'./NotFound/redirectPage';
+import NotFound from './NotFound/redirectPage';
 
 
 const routing = (
     <Router>
         <App />
-
         <div>
             <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/blogs" component={Blogs} />
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/members" component={Members} />
-            <Route path="/pageNotFound" component={NotFound} />
-            <Redirect to='/pageNotFound'/>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/blogs" component={Blogs} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/members" component={Members} />
+                <Route component={NotFound} />
             </Switch>
         </div>
     </Router>
