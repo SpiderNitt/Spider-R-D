7137fa12deeb1d52a9b465a7652136c7bceda033
VishalS99 <s-vishal1999@hotmail.com>
2019-12-11 17:26:44 +0530

    Solved Issues

--- src/HomePage/homepage.jsx
+++ src/HomePage/homepage.jsx
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import Footer from '../Footer/Footer';
-=======
 import Footer from '../Footer/footer-overall';
->>>>>>> a17dd28f8dbce64dbf5b44ac98a1d12ac86609db
 import CoreMembers from './Team/CoreMembers/final_core';
 import Team from './Team/Team/team';
 import About from './About/about';
