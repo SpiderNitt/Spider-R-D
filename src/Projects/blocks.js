aeb5fb222a72a8cae865461d215697131a50cb55
labyrinth-maven <srnandhini1108@gmail.com>
2019-12-31 21:31:07 +0530

    add projects json

--- src/Projects/blocks.js
+++ src/Projects/blocks.js
@@ -24,7 +24,7 @@ import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Details from "./projectmembers.json";
+import Details from "./projectmembersV2.json";
 import { albumStyles } from "./projects.css";
 
 const myStyles = makeStyles(theme => ({
