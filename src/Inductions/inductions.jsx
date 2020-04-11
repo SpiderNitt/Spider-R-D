8ba6b5ad6aca7a967ed3571d0653d1463d63703a
barath99 <exploretechsbk@gmail.com>
2020-04-11 21:52:40 +0530

    Final Code for FAQ with sample Questions

--- src/Inductions/inductions.jsx
+++ src/Inductions/inductions.jsx
@@ -1,45 +1,48 @@
-import React, {Component} from 'react';
-import FooterBar from "../Footer/footer-bottom";
+import React, { useState } from 'react';
+import FAQ from './faq';
 import "./inductions.css";
 
-class Inductions extends Component {
-    render() {
-        return (
-            <div style={{overflowX:"hidden"}}>
-                <h1 className="inductions-header">Got Any Questions? Check if we got them here.</h1>
-                <h2 className="inductions-faqQ">How many domains are there inside Spider?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
-                
-                <h2 className="inductions-faqQ">What are those domains? Can I apply for more than one domain?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
-                
-                <h2 className="inductions-faqQ">What is the mentorship programme ?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
-                
-                <h2 className="inductions-faqQ">Will this be evaluated during the inductions ?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
-                
-                <h2 className="inductions-faqQ">How is spider different from other clubs?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
+function App () {
+    const [faqs, setfaqs] = useState([
+        {
+        question: 'How many domains are there inside Spider?',
+        answer: 'We are planning to have 101 domains.',
+        open: false
+        },
+        {
+        question: 'What are those domains? Can I apply for more than one domain?',
+        answer: 'I dont know! Find it out yourself.',
+        open: false
+        },
+        {
+        question: 'What is the mentorship programme ?',
+        answer: 'Simply for us to enjoy ragging you!.',
+        open: false
+        }
+    ]);
 
-                <h2 className="inductions-faqQ">Is there a limit on number of students to be inducted?</h2>
-                <p className="inductions-ans" >
-                Spider, the Research and Development Club of NIT Trichy is a group of people enthusiastic
-                about technology and innovation. We ideate. </p>
-                <FooterBar />
-            </div>
-        )
+    const toggleFAQ = index => {
+        setfaqs(faqs.map((faq, i) => {
+        if (i === index) {
+            faq.open = !faq.open
+        } else {
+            faq.open = false;
+        }
+        return faq;
+        }))
     }
+
+
+    return (
+        <div className="App">
+            <h1 className="about-header">Got any Questions? Check if we got them answered!</h1>
+            <div className="faqs">
+                {faqs.map((faq, i) => (
+                    <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
+                ))}
+            </div>
+        </div>
+  );
 }
 
-export default Inductions;
+export default App;
